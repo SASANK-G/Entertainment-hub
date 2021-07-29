62c51d57d3905d265b866df535287e61373be47b
SASANK-G <gsbnsrsasank@gmail.com>
2021-07-29 19:58:36 +0530

    getapidata

--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import SingleContent from '../../SingleContent/SingleContent';
+
 
 const Trending = () => {
 
@@ -10,19 +12,32 @@ const Trending = () => {
     const { data } = await axios.get(
     `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
     );
-      console.log(data);
+      
     setcontent(data.results);
 
   };
 
   useEffect(() => {
    fetchTrending();
-  }, [])
+  }, []);
    
 
   return (
     <div>
       <span className="pageTitle">Trending</span>
+      <div className="trending">
+        {
+          content && content.map((c)=><SingleContent 
+          key={c.id}
+          id={c.id}
+          poster={c.poster_path}
+          title={c.title || c.name}
+          date={c.first_air_date || c.release_date}
+          media_type={c.media_type}
+          vote_average={c.vote_average}
+          />)
+        }
+      </div>
     </div>
   )
 }
