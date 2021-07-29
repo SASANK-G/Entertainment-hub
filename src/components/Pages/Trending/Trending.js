bdc639689d001f8f6feff48004910a2f9c7754a1
SASANK-G <gsbnsrsasank@gmail.com>
2021-07-29 19:21:01 +0530

    env

--- src/components/Pages/Trending/Trending.js
+++ src/components/Pages/Trending/Trending.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Trending = () => {
+
+  const [content, setcontent] = useState([]);
+
+
   const fetchTrending = async()=>{
     const { data } = await axios.get(
     `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
     );
+      console.log(data);
+    setcontent(data.results);
+
   };
+
+  useEffect(() => {
+   fetchTrending();
+  }, [])
    
 
   return (
