282dfc012048e80771e863ff3a428f25c4baeb62
SASANK-G <gsbnsrsasank@gmail.com>
2021-08-02 18:50:09 +0530

    styled

--- src/App.js
+++ src/App.js
@@ -8,12 +8,44 @@ import Trending from './components/Pages/Trending/Trending';
 import Movies from './components/Pages/Movies/Movies';
 import Search from './components/Pages/Search/Search';
 import Series from './components/Pages/series/Series';
+import styled from 'styled-components';
+
+
+
+
+const SidebarContainer = styled.div`
+  background-color: #39845a;
+  width: 3.5rem;
+  height: 80vh;
+  top:0;
+  bottom:0;
+  border-radius: 0 30px 30px 0;
+  padding: 1rem 0;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  
+`;
+
+
+
+
+
+
+
+
 
 
 function App() {
   return (
       <BrowserRouter>
        <Header/>
+       <SidebarContainer>
+
+       </SidebarContainer>
         <div className="App">
           <Container>
             <Switch>
