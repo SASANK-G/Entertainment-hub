f0854f7a80eb41febdccf9e60951a0de58a863eb
SASANK-G <gsbnsrsasank@gmail.com>
2021-08-02 19:47:26 +0530

    sidenav-inprogress

--- src/App.js
+++ src/App.js
@@ -9,43 +9,115 @@ import Movies from './components/Pages/Movies/Movies';
 import Search from './components/Pages/Search/Search';
 import Series from './components/Pages/series/Series';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 
 
 
 const SidebarContainer = styled.div`
   background-color: #39845a;
-  width: 3.5rem;
-  height: 80vh;
+  width: 2rem;
+  height: 20vh;
   top:0;
+  
+  right:0;
   bottom:0;
   border-radius: 0 30px 30px 0;
-  padding: 1rem 0;
-  position: absolute;
+  
+  position: fixed;
   display: flex;
-  flex-direction: column;
+  
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  margin:auto;
 
   
 `;
 
 
+const SlickBar = styled.div`
+  color:  green;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  justify-content:space-between ;
+  width:12rem;
+  left: ${(props) => (props.clicked ? "12rem" : "-9rem")};
+  position:fixed;
+  top:0;
+  bottom:0;
+  height:30vh;
+  margin:auto;
+  
+  transition: all 0.5s ease;
+  border-radius: 0 30px 30px 0;
+`;
+{/*width: ${(props) => (props.clicked ? "12rem" : "3.5rem")};*/}
+
+const FirstPart = styled.div`
+width:4rem;
+background-color:red;
+
+
+`;
+
+const SecondPart = styled.div`
+width:4rem;
+background-color:blue;
+display:flex;
+justify-content:flex-end;
+
+
+`;
 
 
 
+
+const Button = styled.button`
+  background-color: black;
+  border: none;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  margin: 0.5rem 0 0 0.5rem;
+  cursor: pointer;
+  
+  
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `;
 
 
 
 
 
 function App() {
-  return (
-      <BrowserRouter>
-       <Header/>
-       <SidebarContainer>
 
-       </SidebarContainer>
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+      
+  setClick(!click);
+  console.log(click);
+}
+
+  return (
+    
+      <BrowserRouter>
+      <SidebarContainer>
+        <SlickBar clicked={click}>
+          <FirstPart>
+                hi
+          </FirstPart>
+            <SecondPart>
+              <Button clicked={click} onClick={() => handleClick()}>
+            </Button>
+            </SecondPart>
+        </SlickBar>
+      </SidebarContainer>
+       <Header/>
+       
         <div className="App">
           <Container>
             <Switch>
