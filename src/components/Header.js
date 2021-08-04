addd23b35f791ad1208c6b278fc3cd74999bd9c9
SASANK-G <gsbnsrsasank@gmail.com>
2021-08-04 20:15:23 +0530

    Fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <span onClick={()=> window.scroll(0,0)} className="header" >Entertainment Hub</span>
+    <span onClick={()=> window.scroll(0,0)} className="header" >Entertainment&nbsp;&nbsp;Hub</span>
   )
 }
 
